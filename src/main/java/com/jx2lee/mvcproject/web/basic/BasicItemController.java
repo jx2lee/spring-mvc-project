6d09ba3c78573a869471ee0b7df02b4236d6596c
jx2lee <jaejun.lee.1991@gmail.com>
2021-09-13 00:08:13 +0900

    섹션 7. 스프링 MVC - 웹 페이지 만들기: 상품 등록 폼 (p.208 - 210)

--- src/main/java/com/jx2lee/mvcproject/web/basic/BasicItemController.java
+++ src/main/java/com/jx2lee/mvcproject/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -36,7 +37,18 @@ public class BasicItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
     }
     
     /**
